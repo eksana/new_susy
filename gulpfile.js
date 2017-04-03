7e3110927f65e9ac3d38e016bb43d7d9d8e428c1
eksana <ser.vajnrub@yandex.ru>
2017-04-03 22:47:41 +0300

    start projekt

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp=require('gulp'),
-sass = require('gulp-sass');
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass_susy', function() {
   return gulp.src('src_test1/sass/**/*.scss')
@@ -8,4 +9,25 @@ gulp.task('sass_susy', function() {
           includePaths: ['node_modules/susy/sass']
       }).on('error', sass.logError))
       .pipe(gulp.dest('src_test1/css'));
+});
+
+/*gulp.task('sass_shoko', function() {
+ /* return gulp.src('src_shokolade/sass/***//*.scss')
+      .pipe(sass({
+          /*outputStyle: 'compressed',*/
+          /*includePaths: ['node_modules/susy/sass']
+      }).on('error', sass.logError))
+      .pipe(gulp.dest('src_shokolade/css'));
+});*/
+
+gulp.task('sass_shoko', function(){ // Создаем таск Sass
+    return gulp.src('src_shokolade/sass/**/*.scss') // Берем источник
+        //.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+         .pipe(sass({
+          /*outputStyle: 'compressed',*/
+          includePaths: ['node_modules/susy/sass']
+      }).on('error', sass.logError))
+       .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+        .pipe(gulp.dest('src_shokolade/css')); // Выгружаем результата в папку app/css
+        //.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
