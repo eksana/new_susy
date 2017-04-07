55badb98a1b64b947f90d3330359f0f87761b565
eksana <ser.vajnrub@yandex.ru>
2017-04-07 16:18:11 +0300

    add 3 sections

--- src_shokolade/js/owl.js
+++ src_shokolade/js/owl.js
@@ -1,4 +1,50 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+  $('# owl-carousel-prize').owlCarousel();
  
 });
+$(document).ready(function() {
+            $('.owl-carousel').owlCarousel({
+                loop:true, //Зацикливаем слайдер
+                //dots:true,
+                //margin:50, //Отступ от элемента справа в 50px
+                nav:true, //Отключение навигации
+                //autoplay:true, //Автозапуск слайдера
+                smartSpeed:1000, //Время движения слайда
+                autoplayTimeout:2000, //Время смены слайда
+                responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:2
+                    },
+                    1000:{
+                        items:3
+                    }
+                }
+            });
+        });
+
+/*$(document).ready(function() {
+            $('#owl-carousel-prize').owlCarousel({
+                loop:true, //Зацикливаем слайдер
+                //dots:true,
+                //margin:50, //Отступ от элемента справа в 50px
+                nav:true, //Отключение навигации
+                autoplay:true, //Автозапуск слайдера
+                smartSpeed:1000, //Время движения слайда
+                autoplayTimeout:2000, //Время смены слайда
+               responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:2
+                    },
+                    1000:{
+                        items:5
+                    }
+                }
+            });
+        });*/
+
