f77a13665bb2cbe1e07a5ab98c61993560f03842
eksana <ser.vajnrub@yandex.ru>
2017-04-08 02:04:40 +0300

    add src_window

--- gulpfile.js
+++ gulpfile.js
@@ -31,3 +31,15 @@ gulp.task('sass_shoko', function(){ // Создаем таск Sass
         .pipe(gulp.dest('src_shokolade/css')); // Выгружаем результата в папку app/css
         //.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
+
+gulp.task('sass_wind', function(){ // Создаем таск Sass
+    return gulp.src('src_window/sass/**/*.scss') // Берем источник
+        //.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+         .pipe(sass({
+          /*outputStyle: 'compressed',*/
+          includePaths: ['node_modules/susy/sass']
+      }).on('error', sass.logError))
+       .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+        .pipe(gulp.dest('src_window/css')); // Выгружаем результата в папку app/css
+        //.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
