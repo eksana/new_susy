ec3fe420d1800190610b28a1d5e441d4052e2fd9
eksana <ser.vajnrub@yandex.ru>
2017-05-24 17:22:34 +0300

    add blockanimate

---
+++ src_blockanimate/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.box4').click(function(){
+		$('.capsAndDish').animate({marginLeft:0});
+
+	});
+	/*$('#cross').click(function(){
+		$('.topNavUpperAside').animate({right:-280});
+
+	});*/
+});
