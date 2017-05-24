967a6de1689b02952900b658493dbe7d2278c284
eksana <ser.vajnrub@yandex.ru>
2017-05-24 22:27:20 +0300

    anim

--- src_blockanimate/js/main.js
+++ src_blockanimate/js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 	$('.box4').click(function(){
-		$('.capsAndDish').animate({marginLeft:0});
+		$('.capsAndDish').animate({marginLeft:0},800);
+		$('.box-opacity').animate({left:0,opacity:0.9},600);
+		$('.inner_anim_left').animate({left:0,opacity:0.9},800);
+  
+		$('.titleFade').animate({top:0},400);
 
 	});
 	/*$('#cross').click(function(){
